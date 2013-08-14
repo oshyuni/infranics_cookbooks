9039e65f28ee26beb685b986498b9460f3046553
huitae-rightscale <huitae.kim@rightscale.com>
2013-08-14 12:11:07 +0900

    second commit

---
+++ cookbooks/srtg/recipes/default.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook Name:: srtg
+# Recipe:: default
+#
+# Copyright 2013, Infranics
+#
+# All rights reserved - Do Not Redistribute
+#
+rightscale_marker :begin
+
+
+# END of RS tag section
+rightscale_marker :end
