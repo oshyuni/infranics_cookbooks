287f96b092754b546f20d34499750d624bc780b3
huitae-rightscale <huitae.kim@rightscale.com>
2013-08-14 12:13:57 +0900

    another commit

--- cookbooks/srtg/metadata.rb
+++
@@ -1,6 +0,0 @@
-maintainer       "Infranics"
-maintainer_email "huitae.kim@rightscale.com"
-license          "All rights reserved"
-description      "Installs/Configures srtg"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
