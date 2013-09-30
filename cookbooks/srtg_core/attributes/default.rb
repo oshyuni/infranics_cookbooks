3f7314d78f32f28e47eec03a34fdae67be30cd9c
oshyuni <oshyuni@infranics.com>
2013-09-30 16:12:39 +0900

    Modified attributes srtg_core

--- cookbooks/srtg_core/attributes/default.rb
+++ cookbooks/srtg_core/attributes/default.rb
@@ -12,7 +12,7 @@ default[:srtg_core][:source_sha256]    = "0c7c1f2603b53ab8c9ebdb9881c4a833aad785
 # install attributes
 default[:srtg_core][:master_ip]        = node[:cloud][:public_ips][0]
 default[:srtg_core][:install_dir]      = "/usr/SysMaster70"
-default[;srtg_core][:java_dir]         = "/usr/SysMaster70/jdk1.6"
+default[:srtg_core][:java_dir]         = "/usr/SysMaster70/jdk1.6"
 default[:srtg_core][:tomcat_dir]       = "/usr/SysMaster70/tomcat5.5"
 default[:srtg_core][:install_file]     = "SysMaster70_Linux*.tar"
 
